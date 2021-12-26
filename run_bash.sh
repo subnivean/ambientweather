dac09a3a20df7739e6499d25b77ccfc3d7313cc9
Mark Krautheim <mark.krautheim@gmail.com>
2021-12-26 19:56:59 +0000

    Fix path to be machine-agnostic

--- run_bash.sh
+++ run_bash.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+TESLA_DATA_PATH=$(dirname $(realpath -s ./data/))
 SCRIPT_PATH=$(dirname $(realpath -s $0))
 
 docker run --rm -it \
   -v $SCRIPT_PATH/data:/data \
+  -v $SCRIPT_PATH/src:/app \
+  -v $TESLA_DATA_PATH:/tesla_data \
    ambientweather-2 /bin/bash
 
---
+++ src/analyze.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import sqlite3
+
+awconn = sqlite3.connect("../data/ambientweather.db")
+AWSQL = "select date, avg(tempf) from dbtable where strftime('%H', date, '-5 hours') < '06' group by date(date, '-5 hours');"
+awdf = pd.read_sql(AWSQL, awconn)
+
+print(awdf)
+
+tsconn = sqlite3.connect("../tesla_data/energy.sqlite")
+TSSQL = "select DateTime,Home_kW,Home_kWh from energy_data;"
+tsdf = pd.read_sql(TSSQL, tsconn)
+
+print(tsdf.tail())
+
+# print(tsdf[tsdf['Home_kW' > 7.3]])
+
+print(tsdf.describe())

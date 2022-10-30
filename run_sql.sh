be36aa81fbf8e30ac6586de4efb1a61ae8e688b2
Mark Krautheim <mark.krautheim@gmail.com>
2022-10-30 16:16:51 +0000

    Remove unused file

--- run_sql.sh
+++ run_sql.sh
@@ -5,9 +5,9 @@ SCRIPT_PATH=$(dirname $(realpath -s $0))
 docker run --rm \
    -v $SCRIPT_PATH/data:/data \
    ambientweather \
-   sqlite3 -readonly -box ../data/ambientweather.db \
+   sqlite3 -readonly -box /data/ambientweather.db \
      "select
-         date,tempf,temp1f,temp4f
+         date,tempf,tempinf
       from
          dbtable
       where

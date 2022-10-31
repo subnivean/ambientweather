4070ef299dbe2458014e3b32ccbecf6276d9046f
Mark Krautheim <mark.krautheim@gmail.com>
2022-10-31 01:21:46 +0000

    Get data for all AmbientWeather devices

--- src/get_ambientweather_data.py
+++ src/get_ambientweather_data.py
@@ -10,59 +10,74 @@ import pandas as pd
 import awdtypes
 import awsecrets
 
-# DEVICENUM = 0  # Old station
-DEVICENUM = 1  # New station
-
 # Need to run this *before* importing AmbientAPI
 os.environ.update(**awsecrets.env)
 
 from ambient_api.ambientapi import AmbientAPI
 
-DBFILE = Path('../data/ambientweather.db')
+DBFILE = Path('/data/ambientweather.db')
+
+def get_devices(api):
+    n = 0
+    while (n := n + 1) <= 5:
+        try:
+            alldevices = api.get_devices()
+            break
+        except (IndexError, ConnectionError):
+            pass
+        # Sleep a little and try again
+        time.sleep(5)
+        # print("Trying again")
+    else:
+        print("System unreachable.")
+        sys.exit()
+
+    return alldevices
+
+
+def add_latest_data_to_db(devicenum, device, conn):
+    lastdata = device.last_data
+    dtypes = awdtypes.get_awdtypes(lastdata)
+
+    wsdata = {k: [v] for k, v in lastdata.items()}
+    wsdata.pop('lastRain')  # Don't care
+
+    for dt in list(dtypes):
+        if dt not in wsdata:
+            dtypes.pop(dt)
+
+    df = pd.DataFrame.from_dict(wsdata).astype(dtypes)
+    df['date'] = pd.to_datetime(df['date'])
+    dftstamp = df['dateutc'].values[0]
+
+    cur = conn.cursor()
+
+    try:
+        cur.execute(f"select dateutc from dbtable{devicenum} "
+                    "order by dateutc desc limit 1")
+        lasttstamp = cur.fetchone()[0]
+    except sqlite3.OperationalError:
+        lasttstamp = -999
+
+    if dftstamp != lasttstamp:
+        df.to_sql(f"dbtable{devicenum}", conn,
+                  if_exists='append', index=False)
+
+    cur.close()
+
+##############################################################################
+##############################################################################
+############################# START MAIN PROGRAM #############################
+##############################################################################
+##############################################################################
 
 api = AmbientAPI()
-
-n = 0
-while n < 5:
-    try:
-        ws = api.get_devices()[DEVICENUM]
-        break
-    except (IndexError, ConnectionError):
-        pass
-
-    # Sleep a little and try again
-    time.sleep(5)
-    n += 1
-    # print("Trying again")
-else:
-    print("System unreachable.")
-    sys.exit()
-
-lastdata = ws.last_data
-dtypes = awdtypes.get_awdtypes(lastdata)
-
-wsdata = {k: [v] for k, v in lastdata.items()}
-wsdata.pop('lastRain')  # Don't care
-
-for dt in list(dtypes):
-    if dt not in wsdata:
-        dtypes.pop(dt)
-
-df = pd.DataFrame.from_dict(wsdata).astype(dtypes)
-df['date'] = pd.to_datetime(df['date'])
-dftstamp = df['dateutc'].values[0]
+alldevices = get_devices(api)
 
 conn = sqlite3.connect(DBFILE)
-cur = conn.cursor()
-try:
-    cur.execute('select dateutc from dbtable order by dateutc desc limit 1')
-    lasttstamp = cur.fetchone()[0]
-except sqlite3.OperationalError:
-    lasttstamp = -999
-cur.close()
 
-if dftstamp != lasttstamp:
-    df.to_sql('dbtable', conn, if_exists='append', index=False)
+for devicenum, device in enumerate(alldevices):
+    add_latest_data_to_db(devicenum, device, conn)
 
 conn.commit()
 conn.close()
